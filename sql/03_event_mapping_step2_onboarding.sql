4895d8ffc0e937c4da2b5a254d73171bb6d66610
Ritvaj Madotra <67282401+ritvaj@users.noreply.github.com>
2025-12-13 23:43:41 +0530

    Update 03_event_mapping_step2_onboarding.sql

--- sql/03_event_mapping_step2_onboarding.sql
+++ sql/03_event_mapping_step2_onboarding.sql
@@ -1,36 +1,22 @@
 Out of all users who opened the app, 603 users proceeded to view an onboarding screen during their first session.
 
-  -- Step 2: Onboarding Screen Viewed (user-level)
+-- Step 2: Onboarding Screen Viewed (user-level)
 
-WITH step1_app_open AS (
-  SELECT
-    user_pseudo_id,
-    MIN(event_timestamp) AS session_start_ts
-  FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20201101`
-  WHERE event_name = 'session_start'
-  GROUP BY user_pseudo_id
-),
-
-onboarding_events AS (
-  SELECT
-    e.user_pseudo_id,
-    e.event_timestamp AS onboarding_ts
-  FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20201101` e,
-       UNNEST(e.event_params) AS ep
-  WHERE e.event_name = 'page_view'
-    AND ep.key = 'page_title'
-    AND ep.value.string_value IN (
-      'Home',
-      'Google Online Store',
-      'The Google Merchandise Store - Log In'
-    )
-)
-
+CREATE OR REPLACE TABLE
+`psyched-hook-481007-m9.taskflowhq_analytics.step2_onboarding_users` AS
 SELECT
   s.user_pseudo_id,
-  MIN(o.onboarding_ts) AS onboarding_ts
-FROM step1_app_open s
-JOIN onboarding_events o
-  ON s.user_pseudo_id = o.user_pseudo_id
- AND o.onboarding_ts > s.session_start_ts
+  MIN(e.event_timestamp) AS onboarding_ts
+FROM `psyched-hook-481007-m9.taskflowhq_analytics.step1_app_open` s
+JOIN `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20201101` e
+  ON s.user_pseudo_id = e.user_pseudo_id
+JOIN UNNEST(e.event_params) ep
+WHERE e.event_name = 'page_view'
+  AND ep.key = 'page_title'
+  AND ep.value.string_value IN (
+    'Home',
+    'Google Online Store',
+    'The Google Merchandise Store - Log In'
+  )
+  AND e.event_timestamp > s.session_start_ts
 GROUP BY s.user_pseudo_id;
