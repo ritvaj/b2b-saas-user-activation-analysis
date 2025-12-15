bf7c2fd83f47a68c0920f4383e8abeb545406fbf
Ritvaj Madotra <67282401+ritvaj@users.noreply.github.com>
2025-12-15 19:32:42 +0530

    Update 08_retention_cohort.sql

--- sql/08_retention_cohort.sql
+++ sql/08_retention_cohort.sql
@@ -22,3 +22,15 @@ JOIN `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*` e
 WHERE
   PARSE_DATE('%Y%m%d', e.event_date)
     = DATE(TIMESTAMP_MICROS(o.onboarding_ts)) + 3;
+
+-- Day 7 Retention
+CREATE OR REPLACE TABLE
+`psyched-hook-481007-m9.taskflowhq_analytics.day7_retention_users` AS
+SELECT DISTINCT
+  o.user_pseudo_id
+FROM `psyched-hook-481007-m9.taskflowhq_analytics.step2_onboarding_users` o
+JOIN `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*` e
+  ON o.user_pseudo_id = e.user_pseudo_id
+WHERE
+  PARSE_DATE('%Y%m%d', e.event_date)
+    = DATE(TIMESTAMP_MICROS(o.onboarding_ts)) + 7;
