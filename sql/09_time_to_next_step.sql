6a42eedd5f95a392688697e331c2c72e49897102
Ritvaj Madotra <67282401+ritvaj@users.noreply.github.com>
2025-12-15 20:00:15 +0530

    Update and rename 09_time_to_next_step to 09_time_to_next_step.sql

---
+++ sql/09_time_to_next_step.sql
@@ -0,0 +1,52 @@
+-- Time from Onboarding to Feature Discovery (per user)
+WITH onboarding AS (
+  SELECT
+    user_pseudo_id,
+    onboarding_ts
+  FROM `psyched-hook-481007-m9.taskflowhq_analytics.step2_onboarding_users`
+),
+
+feature_discovery AS (
+  SELECT
+    user_pseudo_id,
+    MIN(feature_discovery_ts) AS feature_ts
+  FROM `psyched-hook-481007-m9.taskflowhq_analytics.step3_feature_discovery_users`
+  GROUP BY user_pseudo_id
+)
+
+SELECT
+  APPROX_QUANTILES(
+    (feature_ts - onboarding_ts) / 1e6,
+    100
+  )[OFFSET(50)] AS median_seconds_onboarding_to_feature
+FROM onboarding o
+JOIN feature_discovery f
+  ON o.user_pseudo_id = f.user_pseudo_id
+WHERE feature_ts > onboarding_ts;
+
+-- Time from Feature Discovery to Activation
+WITH feature_discovery AS (
+  SELECT
+    user_pseudo_id,
+    MIN(feature_discovery_ts) AS feature_ts
+  FROM `psyched-hook-481007-m9.taskflowhq_analytics.step3_feature_discovery_users`
+  GROUP BY user_pseudo_id
+),
+
+activation AS (
+  SELECT
+    user_pseudo_id,
+    MIN(activation_ts) AS min_activation_ts
+  FROM `psyched-hook-481007-m9.taskflowhq_analytics.step5_activation_users`
+  GROUP BY user_pseudo_id
+)
+
+SELECT
+  APPROX_QUANTILES(
+    (min_activation_ts - feature_ts) / 1e6,
+    100
+  )[OFFSET(50)] AS median_seconds_feature_to_activation
+FROM feature_discovery f
+JOIN activation a
+  ON f.user_pseudo_id = a.user_pseudo_id
+WHERE min_activation_ts > feature_ts;
