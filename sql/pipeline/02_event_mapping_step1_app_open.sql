8d4ce4cc3aa0c67e5003db1fc5d16a89d4acf11d
Ritvaj Madotra <67282401+ritvaj@users.noreply.github.com>
2025-12-28 19:16:01 +0530

    Update 02_event_mapping_step1_app_open.sql

--- sql/pipeline/02_event_mapping_step1_app_open.sql
+++ sql/pipeline/02_event_mapping_step1_app_open.sql
@@ -1,7 +1,6 @@
-We defined the funnel entry as unique users who triggered a session_start event, anchored to their first session timestamp per day.
-
 -- Step 1: App Open (user-level)
-  
+-- This query captures the first `session_start` event per user as the funnel entry point.
+
 CREATE OR REPLACE TABLE
 `psyched-hook-481007-m9.taskflowhq_analytics.step1_app_open` AS
 SELECT
@@ -10,3 +9,5 @@ SELECT
 FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20201101`
 WHERE event_name = 'session_start'
 GROUP BY user_pseudo_id;
+
+-- A total of 2339 users started a session on the selected day.
